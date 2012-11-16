00fbff07018bf7151e637b29cbb8079328cb78ff
Tiago Babo <tiago.babo@gmail.com>
2012-11-16 12:02:57 +0000

    Adds improvments

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// SERVER
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
-  , fs = require('fs')
+  , fs = require('fs');
 
 app.listen(8080);
 
@@ -17,18 +18,26 @@ function handler (req, res) {
 }
 
 sockets = [];
-color_matrix = [100][160];
+color_matrix = new Array (100);
+
+for(i = 0; i < 100; i++) {
+  color_matrix[i] = new Array (160); 
+  for(j = 0; j < 160; j++)
+      color_matrix[i][j] = {color: "#000000", username: "pai natal"};
+}
 
 io.sockets.on('connection', function (socket) {
   sockets.push(socket);
   
   socket.emit('welcome', { matrix: color_matrix });
   
-  socket.on('newPixelFromClient', function (pixelFromClient) {
-    color_matrix[pixelFromClient.y][pixelFromClient.x] = pixelFromClient.color;
+  socket.on('newPixelFromClient', function (data) {
+    color_matrix[data.pixelFromClient.y][data.pixelFromClient.x] = {color: data.pixelFromClient.color, username: data.pixelFromClient.username};
+
     for(i = 0; i < sockets.length; i++)
-      sockets[i].emit('newPixelFromServer', { pixel: pixelFromClient});
+      sockets[i].emit('newPixelFromServer', { pixel: data.pixelFromClient});
   });
+
 });
 
 io.sockets.on('disconnect', function (socket) {
