ee54f0d67d65b0b47ae35a453208f4dce3329011
Tiago Babo <tiagobabo@wifi-interno.fe.up.pt>
2012-11-19 16:01:18 +0000

    Adds final version

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// DATABASE
+var pg = require('pg'); 
+var conString = "pg://postgres:12345@localhost/codebits";
+
+//error handling omitted
+var client = new pg.Client(conString);
+client.connect();
+
 // SERVER
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
@@ -6,7 +14,8 @@ var app = require('http').createServer(handler)
 app.listen(8080);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
+
+fs.readFile(__dirname + '/login.html',
   function (err, data) {
     if (err) {
       res.writeHead(500);
@@ -20,10 +29,10 @@ function handler (req, res) {
 sockets = [];
 color_matrix = new Array (100);
 
-for(i = 0; i < 100; i++) {
+for(i = 0; i < 200; i++) {
   color_matrix[i] = new Array (160); 
-  for(j = 0; j < 160; j++)
-      color_matrix[i][j] = {color: "#000000", username: "pai natal"};
+  for(j = 0; j < 320; j++)
+      color_matrix[i][j] = {color: "#000000", username: "no_avatar"};
 }
 
 io.sockets.on('connection', function (socket) {
@@ -32,10 +41,17 @@ io.sockets.on('connection', function (socket) {
   socket.emit('welcome', { matrix: color_matrix });
   
   socket.on('newPixelFromClient', function (data) {
+
     color_matrix[data.pixelFromClient.y][data.pixelFromClient.x] = {color: data.pixelFromClient.color, username: data.pixelFromClient.username};
+    
+    client.query({
+      name: 'insert pixel',
+      text: "INSERT INTO pixel(x, y, color, username) values($1, $2, $3, $4)",
+      values: [data.pixelFromClient.x, data.pixelFromClient.y, data.pixelFromClient.color, data.pixelFromClient.username]
+    });
 
     for(i = 0; i < sockets.length; i++)
-      sockets[i].emit('newPixelFromServer', { pixel: data.pixelFromClient});
+      sockets[i].emit('newPixelFromServer', { pixelFromServer: data.pixelFromClient});
   });
 
 });
@@ -45,4 +61,20 @@ io.sockets.on('disconnect', function (socket) {
     if(sockets[i] == socket)
       delete sockets[i]
 });
+
+count = 1;
+setTimeout(function() {
+  var query = client.query('SELECT * FROM pixel');
+  query.on('row', function(row) {
+
+    setTimeout(function() {
+      for(i = 0; i < sockets.length; i++) {
+        sockets[i].emit('newPixelFromServer', { pixelFromServer : { x : row.x, y : row.y, color : row.color, username : row.username}});
+
+      }
+      console.log("FOI");
+    }, 1*(count+1));
+    count++;
+    });
+}, 20000);
 
